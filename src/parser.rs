2eead1b9cb0371fa920fffd69c29b101e39b1749
Dmytro Andriievych <andriievych.dmytro@gmail.com>
2025-01-21 22:39:48 +0200

    feat: wip image parsing logic

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,5 @@
 use std::io::{Read, Seek};
+use std::path::PathBuf;
 
 use anyhow::Context;
 use tar::Archive;
@@ -9,7 +10,16 @@ const GZIP_MAGIC_NUMBER: [u8; 2] = [0x1f, 0x8b];
 pub enum BlobType {
     Tar,
     GzippedTar,
+    Json,
 }
+
+#[derive(Debug, Clone)]
+pub struct ChangedFile {
+    path: PathBuf,
+    size: u64,
+}
+
+pub type LayerChangeSet = Vec<ChangedFile>;
 
 #[derive(Debug, Default)]
 pub struct Parser {}
@@ -31,37 +41,62 @@ impl Parser {
             if header.path_bytes().starts_with(b"blobs/sha256/") {
                 // Check if blob is tar/gzipped tar
 
-                let mut buf_offset = 0;
-                let blob_type: Option<BlobType> = loop {
+                let mut filled = 0;
+                let blob_type = loop {
                     let read = entry
-                        .read(&mut tar_header[buf_offset..])
+                        .read(&mut tar_header[filled..])
                         .context("failed to get the beginning of a blob")?;
 
+                    filled += read;
                     match read {
-                        0 => break None,
+                        0 => break BlobType::Json,
                         1.. => {
-                            if buf_offset + read < 2
-                                || (tar_header[..2] != GZIP_MAGIC_NUMBER && buf_offset + read < 262)
-                            {
+                            if filled < 2 || (tar_header[..2] != GZIP_MAGIC_NUMBER && filled != 262) {
                                 // We need more data
-                                buf_offset = read;
                                 continue;
                             }
                         }
                     }
 
                     if &tar_header[257..262] == b"ustar" {
-                        break Some(BlobType::Tar);
+                        break BlobType::Tar;
                     }
 
                     if tar_header[..2] == GZIP_MAGIC_NUMBER {
-                        break Some(BlobType::GzippedTar);
+                        break BlobType::GzippedTar;
                     }
 
-                    break None;
+                    break BlobType::Json;
                 };
 
                 dbg!(blob_type);
+
+                // Restore the original entry so that it gets parsed correctly
+                let entry = tar_header[..filled].chain(entry);
+
+                match blob_type {
+                    BlobType::Tar => self.parse_tar_layer(entry).context("error while parsing a tar layer")?,
+                    BlobType::GzippedTar => todo!(),
+                    BlobType::Json => todo!(),
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn parse_tar_layer<R: Read>(&self, src: R) -> anyhow::Result<()> {
+        let mut archive = Archive::new(src);
+
+        let mut change_set = LayerChangeSet::new();
+        for entry in archive.entries().context("failed to get entries from the archive")? {
+            let entry = entry.context("error while reading an entry")?;
+            let header = entry.header();
+            if let Ok(path) = header.path() {
+                change_set.push(ChangedFile {
+                    path: path.into_owned(),
+                    size: header.size().unwrap_or(0),
+                })
             }
         }
 
